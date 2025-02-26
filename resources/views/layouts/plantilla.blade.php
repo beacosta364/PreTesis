f6ce09b57fa977263869593b165ad98938ccba5e
Brayan Estein Acosta Achinte <brayanacosta364@gmail.com>
2025-02-26 00:05:47 -0500

    modificación plantilla, traduccion profile

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -15,14 +15,23 @@
     <link rel="stylesheet" href="{{ url('/css/estilos-formularios.css') }}">
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 
     
 </head>
 <body>
     <!-- slidebar -->
     <aside class="slidebar" id="slidebar">
-        <h2>My tienda</h2>
+        <!-- <h2>My tienda</h2> -->
+        <span class="logooo"><img src="{{asset('img/ABY.png')}}" alt="LOGO"></span>
+
+        <!-- Logo Empresa -->
+        <div class="element-slidebar">
+            <div class="element-slidebar-btn profile">
+                <span><img src="{{asset('img2/LogoLaPapa.png')}}" alt="Logo"></span>
+                <p>La Papa</p>
+            </div>
+        </div>
         <!-- Perfil -->
          <div class="element-slidebar">
             <div class="element-slidebar-btn profile">
@@ -31,7 +40,7 @@
                 <p>{{Auth::user()->name}}</p>
             </div>
             <div class="element-slidebar-content">
-                <a href="{{'profile.edit'}}">Perfil</a>
+                <a href="{{'profile'}}">Perfil</a>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
@@ -39,76 +48,85 @@
                 </form>
             </div>
         </div>
-        <!-- Productos -->
+
+        <!-- Gestion del sistema -->
+        
         <div class="element-slidebar">
             <div class="element-slidebar-btn">
-                <!-- <span><img src="img/rokrt.png" alt="Product"></span> -->
-                <span><img src="{{asset('img/rokrt.png')}}" alt="Product"></span>
-                <!-- <span><img src="{{ asset('storage/rokrt.jpg') }}" alt="Product"></span> -->
-                <p>Productos</p>
+                <span><img src="{{asset('img/GestionDelSistema.png')}}" alt="Gestion del sistema"></span>
+                <p>Gestion del sistema</p>
             </div>
             <div class="element-slidebar-content">
-            <div class="element-slidebar-content">
-                
-                 <a href="">Todos</a>
-                 <a href="">Agregar</a>
-            </div>
+                <a href="">Gestion de inventarios</a>
+                <a href="">Ir a la gestión de alarma y acceso</a>
+                <a href="">Lista de usuarios registrados</a>
+                <a href="" class="btn btn-warning">Ver Productos Agotados o Por Agotarse</a>
             </div>
         </div>
+
+
+        <!-- Productos -->
+            <div class="element-slidebar">
+                <div class="element-slidebar-btn">
+                    <!-- <span><img src="{{asset('img/rokrt.png')}}" alt="Product"></span> -->
+                    <span><img src="{{ asset('img/Productos.png') }}" alt="Gestion de productos"></span>
+                    <p>Productos</p>
+                </div>
+                <div class="element-slidebar-content">
+                    
+                    <a href="">Todos</a>
+                    <a href="">Agregar</a>
+                </div>
+            </div>
 
         <!-- Categorias -->
-        <div class="element-slidebar">
-            <div class="element-slidebar-btn">
-                <span><img src="{{asset('img/icono1.png')}}" alt="Product"></span>
-                <p>Categorias</p>
+            <div class="element-slidebar">
+                <div class="element-slidebar-btn">
+                    <!-- <span><img src="{{asset('img/icono1.png')}}" alt="Product"></span> -->
+                    <span><img src="{{asset('img/Categoria.png')}}" alt="Gestion del categorias"></span>
+                    <p>Categorias</p>
+                </div>
+                <div class="element-slidebar-content">
+                    <a href="{{ route('categoria.index') }}">Todos</a>
+                    <a href="{{ route('categoria.create') }}">Agregar</a>
+                </div>
             </div>
-            <div class="element-slidebar-content">
-            <div class="element-slidebar-content">
-            <a href="{{ route('categoria.index') }}">Todos</a>
-                <a href="{{ route('categoria.create') }}">Agregar</a>
-            </div>
-            </div>
-        </div>
 
-        <!-- Provedores -->
-        <div class="element-slidebar">
-            <div class="element-slidebar-btn">
-                <!-- <span><img src="img/provedores.png" alt="provedor"></span> -->
-                <span><img src="{{asset('img/provedores.png')}}" alt="provedor"></span>
-                
-                <p>Provedores</p>
+        <!-- Monitoreo y seguridad -->
+            <div class="element-slidebar">
+                <div class="element-slidebar-btn">
+                    <span><img src="{{asset('img/MonitoreodeSeguridad.png')}}" alt="Monitoreo y seguridad"></span>
+                    <p>Monitoreo y seguridad</p>
+                </div>
+                <div class="element-slidebar-content">    
+                    <a href="">Auditoria</a>
+                    <a href="">Notificaciones</a>
+                </div>
             </div>
-            <div class="element-slidebar-content">
-                <a href="">Todos</a>
-                <a href="">Agregar</a>
-            </div>
-        </div>
 
-        <!-- Comptras -->
-        <div class="element-slidebar">
-            <div class="element-slidebar-btn">
-                <!-- <span><img src="img/compras.png" alt="compras"></span> -->
-                <span><img src="{{asset('img/compras.png')}}" alt="compras"></span>
-                <p>Comptras</p>
-            </div>
-            <div class="element-slidebar-content">
-                <a href="">Todos</a>
-                <a href="">Agregar</a>
+            <!-- Configuración y soporte -->
+            <div class="element-slidebar">
+                <div class="element-slidebar-btn">
+                    <span><img src="{{asset('img/ConfiguracionyReportes.png')}}" alt="Configuración y soporte"></span>
+                    <p>Configuración y soporte</p>
+                </div>
+                <div class="element-slidebar-content">
+                    <a href="">Configuración</a>
+                    <a href="">Soporte y documentación</a>
+                </div>
             </div>
 
 
-        </div>
-        <!-- Ventas -->
+            <!-- Reportes -->  
         <div class="element-slidebar">
             <div class="element-slidebar-btn">
-                <!-- <span><img src="img/ventas.png" alt="ventas"></span> -->
-                <span><img src="{{asset('img/ventas copy.png')}}" alt="ventas"></span>
-                <p>Ventas</p>
+                <span><img src="{{asset('img/Reportes.png')}}" alt="Reportes"></span>
+                <p>Reportes</p>
             </div>
-            <div class="element-slidebar-content">
-                <a href="">Todos</a>
-                <a href="">Agregar</a>
-            </div>
+            <div class="element-slidebar-content">         
+                <a href="">Reportes de inventarios</a>
+                <a href="">Reportes de movimientos</a>
+            </div>     
         </div>
     </aside>
     <!-- main -->
