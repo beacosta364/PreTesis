a4ea0c77d376735d104073a3f0af89807e1d3f01
Brayan Estein Acosta Achinte <brayanacosta364@gmail.com>
2025-05-29 10:13:57 -0500

    correccion menu hamburguer app inventary inside administration

--- public/js/script.js
+++ public/js/script.js
@@ -5,8 +5,31 @@ document.querySelectorAll('.element-slidebar-btn').forEach(btn=> {
     });
 });
 
+
+document.addEventListener('click', function(event) {
+    const sidebar = document.querySelector('.slidebar');
+    const menuButton = document.querySelector('.menu-hamburger');
+
+    // Si el menú está visible
+    if (sidebar.classList.contains('hidden')) {
+        // Si el clic fue fuera del sidebar y fuera del botón del menú
+        if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+            sidebar.classList.remove('hidden');
+        }
+    }
+});
+
+// Mostrar el sidebar cuando se hace clic en el botón
+document.querySelector('.menu-hamburger').addEventListener('click', function(e) {
+    const sidebar = document.querySelector('.slidebar');
+    sidebar.classList.toggle('hidden');
+    e.stopPropagation();
+});
+
+
 // Toggle para el menú
- document.getElementById('menu-toggle').addEventListener('click', function() {
-     let slidebar = document.getElementById('slidebar');
-     slidebar.classList.toggle('hidden');
- });
+//  document.getElementById('menu-toggle').addEventListener('click', function() {
+//      let slidebar = document.getElementById('slidebar');
+//      slidebar.classList.toggle('hidden');
+//  });
+
